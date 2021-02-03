00f26493305eb2a8cc3b27b5f1c119d4a8f996d0
PatoGordo <59514690+PatoGordo@users.noreply.github.com>
2021-02-02 21:56:34 -0300

    Update firebase.py

--- src/services/firebase.py
+++ src/services/firebase.py
@@ -1,10 +1,10 @@
 import pyrebase
 
 config = {
-  "apiKey": "AIzaSyCeLPgM8XD0RyPA3c4CdmO-Eg8y2LuY4xM",
-  "authDomain": "contact-me-pg.firebaseapp.com",
-  "databaseURL": "https://contact-me-pg-default-rtdb.firebaseio.com",
-  "storageBucket": "contact-me-pg.appspot.com"
+  "apiKey": "",
+  "authDomain": "",
+  "databaseURL": "",
+  "storageBucket": ""
 }
 
 firebase = pyrebase.initialize_app(config)
